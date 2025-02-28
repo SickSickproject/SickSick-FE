ae22ae7107f6a613680dda031b05dfdea8704052
5eungwon <kimseung0630@naver.com>
2025-02-28 22:59:56 +0900

    ✨ 헤더바 페이지 구현

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -6,27 +6,49 @@ import sicksickbtn from "../assets/sicksickbtn.jpg"
 
 
 const Mainpage = ()=> {
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const hideSidebar = location.pathname === "/main/three"
-    
 
     const img1 = sicksickbtn
 
-    const [pagestate , setpagestate] = useState("one")
+    const location = useLocation(); // 현재 경로 정보 가져오기
+    const path = location.pathname; // 예: "/main/one"
+  
+    // 경로에서 마지막 부분만 추출 ("/main/one" -> "one")
+    const lastSegment = path.split("/").pop();
+
+
+    const [btnclick,setbtnclick] = useState(lastSegment === "one" ? [1,0,0] : [0,1,0])
+    console.log(btnclick)
+    const [Left,setLeft] = useState(window.innerWidth*0.8)
+    const navigate = useNavigate();
+
+    const updateWidth = ()=>{
+        const screenWidth = window.innerWidth; // 전체 화면 너비
+        setLeft(screenWidth * 0.8);
+    }
+
+    window.addEventListener("resize",()=>{
+        updateWidth();
+    })
+
+    const btnchange = (i)=>{
+        const newarr = [0,0,0]
+        newarr[i] = 1;
+        setbtnclick(newarr)
+        if(i === 0)navigate(`/main/one`,{replace : true})
+        else if(i === 1)navigate(`/main/two`,{replace : true})
+        else navigate(`/main/three`,{replace : true})
+        
+    }
     return <>
         <Navbar>
-            <div style={{left:"1%",backgroundColor:"yellow", width:"5%",height:"80%",position:"absolute"}} onClick={()=>{console.log(1)}}>
-                <img src={img1} style={{width:"100%",height:"100%"}}></img>
-            </div>
-            <Btns style={{left:"75%"}} onClick={()=>navigate(`/main/one`,{replace : true})}>식식</Btns>
-            <Btns style={{left:"82%"}} onClick={()=>navigate(`/main/two`,{replace : true})}>고백들</Btns>
-            <Btns style={{left:"89%",width:"7%"}} onClick={()=>navigate(`/main/three`,{replace : true})}>연대하기</Btns>
+            <Imgcontainer onClick={()=>navigate(`/`,{replace : true})} src={img1}></Imgcontainer>
+            <Navbarin left = {Left}>
+                <Btns clicked={btnclick[0]} onClick={()=>{btnchange(0)}} style={{width:"70px"}}>식식</Btns>
+                <Btns clicked={btnclick[1]} onClick={()=>{btnchange(1)}}>고백들</Btns>
+                <Btns clicked={btnclick[2]} onClick={()=>{btnchange(2)}}>연대하기</Btns>
+            </Navbarin>
         </Navbar>
-        { !hideSidebar &&<Sidebar></Sidebar>}
         <Outlet></Outlet>
-    
     </>
 }
 
@@ -37,34 +59,44 @@ position: relative;
 display: flex;
 align-items: center;
 flex-direction: row;
-width:100%;
-height:10%;
-border-bottom: 1px solid black;
+width:1920px;
+height:95px;
+border-bottom: 2px solid black;
 z-index: 9999;
-`;
-
-const Sidebar = styled.div`
-float:left;
-width:17%;
-height:89%;
-background-color:white;
-z-index: 9999;
+box-sizing: border-box;
 `;
 
 const Btns = styled.div`
-width:5%;
-height:50%;
-position:absolute;
+width:100px;
+height:21px;
 display:flex;
 align-items: center;
+margin-right:30px;
 justify-content:center;
 font-size: 24px;
 font-style: normal;
-font-weight: 500;
+font-weight: ${(props)=>(props.clicked ? "800":"500")};
+text-decoration: ${(props)=>(props.clicked ? "underline":"none")};
 &:hover {
     text-decoration: underline;
     cursor:pointer;
     font-weight: 800;
-
   }
+`
+
+const Navbarin = styled.div`
+left: ${(props) => props.left}px;
+width:380px;
+height:95px;
+position:absolute;
+display:flex;
+flex-direction:row;
+align-items:center;
+`
+
+const Imgcontainer = styled.img`
+width:73px;
+height:60px;
+position:absolute;
+left:19px;
 `
