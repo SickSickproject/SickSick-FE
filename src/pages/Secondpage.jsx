28c6672181c4033ce94385a1ace33a7dd791f5a8
5eungwon <kimseung0630@naver.com>
2025-03-03 13:51:51 +0900

    ✨ 드롭다운 애니메이션 수정

--- src/pages/Secondpage.jsx
+++ src/pages/Secondpage.jsx
@@ -33,9 +33,9 @@ const Secondpage = ()=>{
             <SidebarBtn onClick={()=>{setisdropdownopen(!isdropdownopen)}}>인터뷰이</SidebarBtn>
             <AnimatePresence>
                 {isdropdownopen && <motion.div
-                 initial={{ opacity: 0, y: -10 }}
-                 animate={{ opacity: 1, y: 0 }}
-                 exit={{ opacity: 0, y: -10 }}
+                 initial={{ opacity: 0}}
+                 animate={{ opacity: 1}}
+                 exit={{ opacity: 0}}
                  transition={{ duration: 0.3, ease: "easeInOut" }}
                  className="absolute mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg"
                 >
