a83c5c89aba763d43ae315d58b17c990f8bd72ab
5eungwon <kimseung0630@naver.com>
2025-03-23 22:52:47 +0900

    💄 코드 수정

--- src/pages/videolistpage/Leesunminvideo_grid.jsx
+++ src/pages/videolistpage/Leesunminvideo_grid.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components"
 import { useMediaQuery } from "react-responsive";
 import Leesunmin from "../../datas/LeesuminVideoimg";
+import { useNavigate } from "react-router-dom";
 
-const Leesunminvideo_grid = () => {
+const Leesunminvideo_grid = ({setisoverlay,setoverlayinfo}) => {
 
     const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+
+    const navigate = useNavigate();
 
     return <>
 
@@ -13,7 +16,9 @@ const Leesunminvideo_grid = () => {
                 <Namebar>&nbsp;&nbsp;{">"} 이선민</Namebar>
                 <Videocontainer>
                 {Leesunmin.map((v, i) => {
-                        return <Videoelement onClick={() => { console.log(11) }}>
+                        return <Videoelement onClick={() => {
+                            setoverlayinfo({title:v.title,name:v.name,time:v.time,youtubeid:v.youtubeid}); setisoverlay(true); window.scrollTo({ top: 0 }); 
+                         }}>
                              <img src={v.image} style={{ top: "5%",width:"100%", position: "absolute" }}></img>
                              <Videoelement_title>{v.title}</Videoelement_title>
                              <div style={{ width: "90%", height: "1.5%", position: "absolute", top: "95%",fontSize:"0.9vw"}}>인터뷰이 | {v.name}</div>
