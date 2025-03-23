501b2bb04c35a0bb4ddf3ae2d8d753de2410861e
5eungwon <kimseung0630@naver.com>
2025-03-23 22:49:51 +0900

    🍱데이터 수정

--- src/datas/LeejinsolVideoimg.js
+++ src/datas/LeejinsolVideoimg.js
@@ -6,70 +6,92 @@ import LeejinsolVideoimg from "../assets/Videoimg/LeejinsolVideoimg.png"
 
 const Leejinsol = [
     {   
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"안녕하세요. 이진솔입니다.",
-        stitle:`①소개`
+        stitle:`①소개`,
+        time:"41초",
+        youtubeid:"IacTiqQMOj8"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애와 고백의 연결점은 무엇인가요?",
-        stitle:"② 고백"
+        stitle:"② 고백",
+        time:"40초",
+        youtubeid:"U81BZxvWjfI"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애란 어떤 질병인가요?",
-        stitle:"③ 정의"
+        stitle:"③ 정의",
+        time:"1분 1초",
+        youtubeid:"1m5Xt57bXcc"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애를 바라보는 세상의 시선, 무엇이 잘못되었나요?",
-        stitle:"④ 오해와진실"
+        stitle:"④ 오해와진실",
+        time:"1분",
+        youtubeid:"nLd7BLAdgPk"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애의 끝이 있다고 믿으시나요? 그 끝은 어떤 모습일까요?",
-        stitle:"⑤ 완치"
+        stitle:"⑤ 완치",
+        time:"1분 36초",
+        youtubeid:"FId6nSATD3k"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"최근 섭식장애의 양상은 어떻게 나타나고 있나요?",
-        stitle:"⑥ 노력"
+        stitle:"⑥ 노력",
+        time:"1분 18초",
+        youtubeid:"_QLhRgXJlW4"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애 딸을 둔 어머니와 만남을 기획하셨다고 들었는데, 그 이유는 무엇인가요?",
-        stitle:"⑦ 주변 사람들"
+        stitle:"⑦ 주변 사람들",
+        time:"1분 45초",
+        youtubeid:"IwDqCEnXirw"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애 치료 체계에서 느끼는 문제점은 무엇인가요?",
-        stitle:"⑧ 치료"
+        stitle:"⑧ 치료",
+        time:"1분 26초",
+        youtubeid:"7rBFYCQaoUg"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애로부터 씩씩해질 수 있는 방법은 무엇인가요?",
-        stitle:"⑨ 함께 살아가기"
+        stitle:"⑨ 함께 살아가기",
+        time:"1분 32초",
+        youtubeid:"1ufrHe_LVZk"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애로부터 나아지고있다라고 느끼는 순간이 있다면 언제인가요?",
-        stitle:"⑩ 회복"
+        stitle:"⑩ 회복",
+        time:"1분 3초",
+        youtubeid:"L_VL6TKvJ_s"
     },
     {
-        name:"이선민",
+        name:"이진솔",
         image:LeejinsolVideoimg,
         title:"섭식장애로 힘들어하는 사람들에게 전하고 싶은 메시지는 무엇인가요?",
-        stitle: "+ 메세지"
+        stitle: "+ 메세지",
+        time:"1분 59초",
+        youtubeid:"9EmL419nhPM"
     },
 ]
 
--- src/datas/LeesuminVideoimg.js
+++ src/datas/LeesuminVideoimg.js
@@ -6,67 +6,89 @@ const Leesunmin = [
         name:"이선민",
         image:LeesunminVideoimg,
         title:"안녕하세요. 이선민입니다.",
-        stitle:`①소개`
+        stitle:`①소개`,
+        time:"1분 39초",
+        youtubeid:"RkwPC1qc_LY"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"섭식장애와 함께 보낸 10대 시절의 이야기를 들려주실 수 있나요?",
-        stitle:"② 과거"
+        stitle:"② 과거",
+        time:"2분 5초",
+        youtubeid:"0NkTsRISPEQ"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"본격적으로 치료를 결심하게 된 계기가 무엇이었나요?",
-        stitle:"③ 결심"
+        stitle:"③ 결심",
+        time:"2분 4초",
+        youtubeid:"ZzZ34LabuMo"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"아직까지 남아있는 후유증이 있다면?",
-        stitle:"④ 후유증"
+        stitle:"④ 후유증",
+        time:"57초",
+        youtubeid:"--M7dqBNSm4"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"‘혼자가 아니다’라는 위로를 받을 때는 언제였나요?",
-        stitle:"⑤ 위로"
+        stitle:"⑤ 위로",
+        time:"2분 10초",
+        youtubeid:"B_yq0L-o9sI"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"섭식장애를 극복하기 위해 어떤 노력을 해오셨고, 또 하고 있는지...",
-        stitle:"⑥ 노력"
+        stitle:"⑥ 노력",
+        time:"1분 57초",
+        youtubeid:"-vJ1PMSiFso"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"다양한 섭식장애 세미나를 진행하면서 기억에 남는 에피소드가 있을까요?",
-        stitle:"⑦ 기억"
+        stitle:"⑦ 기억",
+        time:"1분 46초",
+        youtubeid:"uSIAU7Ha0CY"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"사회적 분위기에 휩쓸리지 않고 주도적으로 사고하려면 어떻게 해야할까요?",
-        stitle:"⑧ 주도적 사고"
+        stitle:"⑧ 주도적 사고",
+        time:"1분 44초",
+        youtubeid:"jIoRQq20RqE"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"변해가는 내 몸을 어떻게 하면 사랑할 수 있을까요?",
-        stitle:"⑨ 나를 사랑하기"
+        stitle:"⑨ 나를 사랑하기",
+        time:"2분 33초",
+        youtubeid:"x7dVI-eoyAU"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"만약, 아이가 성장하면서 섭식장애와 관련한 고민을 가지게 된다면?",
-        stitle:"⑩ 아이와 섭식장애"
+        stitle:"⑩ 아이와 섭식장애",
+        time:"1분 9초",
+        youtubeid:"f09YYKaew0"
     },
     {
         name:"이선민",
         image:LeesunminVideoimg,
         title:"섭식장애를 겪거나 이겨내는 과정에 있는 사람들에게 전하고 싶은 메세지",
-        stitle: "+ 메세지"
+        stitle: "+ 메세지",
+        time:"1분 11초",
+        youtubeid:"NRcSozxVNtg"
     },
 ]
 
