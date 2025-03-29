b73332716d684c441c8e444263d522c1f8034885
5eungwon <kimseung0630@naver.com>
2025-03-29 21:28:39 +0900

    📝 데이터파일 수정

--- src/datas/LeesuminVideoimg.js
+++ src/datas/LeesuminVideoimg.js
@@ -8,7 +8,8 @@ const Leesunmin = [
         title:"안녕하세요. 이선민입니다.",
         stitle:`①소개`,
         time:"1분 39초",
-        youtubeid:"RkwPC1qc_LY"
+        youtubeid:"RkwPC1qc_LY",
+        id:0
     },
     {
         name:"이선민",
@@ -16,7 +17,8 @@ const Leesunmin = [
         title:"섭식장애와 함께 보낸 10대 시절의 이야기를 들려주실 수 있나요?",
         stitle:"② 과거",
         time:"2분 5초",
-        youtubeid:"0NkTsRISPEQ"
+        youtubeid:"0NkTsRISPEQ",
+        id:1
     },
     {
         name:"이선민",
@@ -24,7 +26,8 @@ const Leesunmin = [
         title:"본격적으로 치료를 결심하게 된 계기가 무엇이었나요?",
         stitle:"③ 결심",
         time:"2분 4초",
-        youtubeid:"ZzZ34LabuMo"
+        youtubeid:"ZzZ34LabuMo",
+        id:2
     },
     {
         name:"이선민",
@@ -32,7 +35,8 @@ const Leesunmin = [
         title:"아직까지 남아있는 후유증이 있다면?",
         stitle:"④ 후유증",
         time:"57초",
-        youtubeid:"--M7dqBNSm4"
+        youtubeid:"--M7dqBNSm4",
+        id:3
     },
     {
         name:"이선민",
@@ -40,7 +44,8 @@ const Leesunmin = [
         title:"‘혼자가 아니다’라는 위로를 받을 때는 언제였나요?",
         stitle:"⑤ 위로",
         time:"2분 10초",
-        youtubeid:"B_yq0L-o9sI"
+        youtubeid:"B_yq0L-o9sI",
+        id:4
     },
     {
         name:"이선민",
@@ -48,7 +53,8 @@ const Leesunmin = [
         title:"섭식장애를 극복하기 위해 어떤 노력을 해오셨고, 또 하고 있는지...",
         stitle:"⑥ 노력",
         time:"1분 57초",
-        youtubeid:"-vJ1PMSiFso"
+        youtubeid:"-vJ1PMSiFso",
+        id:5
     },
     {
         name:"이선민",
@@ -56,7 +62,8 @@ const Leesunmin = [
         title:"다양한 섭식장애 세미나를 진행하면서 기억에 남는 에피소드가 있을까요?",
         stitle:"⑦ 기억",
         time:"1분 46초",
-        youtubeid:"uSIAU7Ha0CY"
+        youtubeid:"uSIAU7Ha0CY",
+        id:6
     },
     {
         name:"이선민",
@@ -64,7 +71,8 @@ const Leesunmin = [
         title:"사회적 분위기에 휩쓸리지 않고 주도적으로 사고하려면 어떻게 해야할까요?",
         stitle:"⑧ 주도적 사고",
         time:"1분 44초",
-        youtubeid:"jIoRQq20RqE"
+        youtubeid:"jIoRQq20RqE",
+        id:7
     },
     {
         name:"이선민",
@@ -72,7 +80,8 @@ const Leesunmin = [
         title:"변해가는 내 몸을 어떻게 하면 사랑할 수 있을까요?",
         stitle:"⑨ 나를 사랑하기",
         time:"2분 33초",
-        youtubeid:"x7dVI-eoyAU"
+        youtubeid:"x7dVI-eoyAU",
+        id:8
     },
     {
         name:"이선민",
@@ -80,7 +89,8 @@ const Leesunmin = [
         title:"만약, 아이가 성장하면서 섭식장애와 관련한 고민을 가지게 된다면?",
         stitle:"⑩ 아이와 섭식장애",
         time:"1분 9초",
-        youtubeid:"f09YYKaew0"
+        youtubeid:"f09YYKaew0",
+        id:9
     },
     {
         name:"이선민",
@@ -88,7 +98,8 @@ const Leesunmin = [
         title:"섭식장애를 겪거나 이겨내는 과정에 있는 사람들에게 전하고 싶은 메세지",
         stitle: "+ 메세지",
         time:"1분 11초",
-        youtubeid:"NRcSozxVNtg"
+        youtubeid:"NRcSozxVNtg",
+        id:10
     },
 ]
 
